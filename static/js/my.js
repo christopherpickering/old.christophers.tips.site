5ee4daa45f33792bc5d9bd77d9f418c16075f0c3
Christopher Pickering <pher122s@gmail.com>
2018-10-21 15:32:48 -0500

    updated commit

---
+++ static/js/my.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(e) {
+console.log('started script')
+$('body').on('click', '.js-toolbar-action', function(event) {
+    event.preventDefault();
+    console.log('clicked')
+        var $img = $('.book');
+        if (!$img.hasClass('with-summary')) {
+            $img.addClass('with-summary');
+        } else {
+            $img.removeClass('with-summary');
+        }
+    });
+
+});
